5418ff2476e9f0d4f7b8fa3d680a15bc8710d925
Shovelopolis <cjogoalie27@gmail.com>
2022-09-07 15:16:40 -0400

    loop and converter made

--- idk.java
+++ idk.java
@@ -1,6 +1,21 @@
-public class idk {
+import java.util.Scanner;
+class idk {
     public static void main(String[] args) {
 
+        boolean run = true;
+        while (run = true) {
+            Scanner myObj = new Scanner(System.in);
+            int tempF;
 
+            // Enter username and press Enter
+            System.out.println("Enter temperature(°F)");
+            tempF = Integer.parseInt(myObj.nextLine());
+
+            int tempC = (int) ((tempF - 32) * 0.5556);
+
+            System.out.print("Temperature(°C) is: ");
+            System.out.println(tempC);
+
+        }
     }
 }
